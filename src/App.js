6a97f4f45cc547424e708eec322e6abfea52cc24
Nikola <nikola@example.com>
2022-03-12 17:27:07 +0000

    mock axios resolves promise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './comp/Header';
-import Clickers from './comp/Clickers';
 import Fetch from './comp/Fetch'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Clickers />
       <Fetch />
     </div>
   );
--- src/comp/Fetch.test.js
+++ src/comp/Fetch.test.js
@@ -1,30 +1,26 @@
 import React from "react";
 import {
   render,
-  cleanup,
-  fireEvent,
   waitForElement
 } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-import axiosMock from "axios";
-import Fetch from "./Fetch";
+import axios from 'axios';
 
-afterEach(cleanup);
+import Fetch from "./Fetch";
 
 const greetingMock = "hello from axiosMock"
 
 it("fetches and displays data", async () => {
-    //We'll be explicit about what data Axios is to return when `get` is called.
-    axiosMock.get.mockResolvedValueOnce({ data: { greeting: greetingMock } });
-  
-    const url = "/greeting";
-    const { getByTestId } = render(<Fetch url={url} />);
-  
-    expect(getByTestId("loading").textContent).toBe("Loading data...");
-    const resolvedSpan = await waitForElement(() => getByTestId("resolved"));
-  
-    expect(resolvedSpan.textContent).toBe(greetingMock);
-    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+  const mockAxiosGet = axios.get.mockImplementation(() => Promise.resolve({ data: { greeting: greetingMock } }));
 
-  });
+  const url = "/greeting";
+  const { getByTestId } = render(<Fetch url={url} />);
+
+  expect(getByTestId("loading").textContent).toBe("Loading data...");
+  const resolvedSpan = await waitForElement(() => getByTestId("resolved"));
+
+  expect(resolvedSpan.textContent).toBe(greetingMock);
+  expect(mockAxiosGet).toHaveBeenCalledTimes(1);
+
+});

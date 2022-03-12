d2bce85dc6142a33d26cb4b1fc338988992285b7
Nikola <nikola@example.com>
2022-03-12 22:00:44 +0000

    default and named exports

--- src/comp/greeterDefault.test.js
+++ src/comp/greeterDefault.test.js
@@ -1,31 +1,46 @@
-import defaultMethod, { method1, method2, method3 } from "./greeterDefault";
+const myModule = jest.requireActual("./greeterDefault");
 
-jest.mock('./greeterDefault', () => ({
-  __esModule: true,
-  default: jest.fn().mockImplementation(() => 'You have called a mocked default method!'),
-  method1: jest.fn().mockImplementation(() => 'You have called a mocked method 1!'),
-  method2: jest.fn().mockImplementation(() => 'You have called a mocked method 2!'),
-  method3: jest.fn().mockImplementation(() => 'You have called a mocked method 3!'),
-}))
+const spyDefault = jest.spyOn(myModule, "default").mockImplementation(() => 5);
+const spyMethod1 = jest.spyOn(myModule, "method1").mockImplementation(() => 23);
 
-describe("default and named exports - module factory with mock implementation mocked file", () => {
-  it("returns the correct value for Method 1", () => {
-    expect(method1()).toBe("You have called a mocked method 1!");
-    expect(method1).toHaveBeenCalledTimes(1);
-  });
+it("spy on default", () => {
+  expect(spyDefault()).toBe(5);
+})
 
-  it("returns the correct value for Method 2", () => {
-    expect(method2()).toBe("You have called a mocked method 2!");
-    expect(method2).toHaveBeenCalledTimes(1);
-  });
+it("spy on method1", () => {
+  expect(spyMethod1()).toBe(23);
+})
 
-  it("returns the correct value for Method 3", () => {
-    expect(method3()).toBe("You have called a mocked method 3!");
-    expect(method3).toHaveBeenCalledTimes(1);
-  });
+// or
 
-  it("returns the correct value for the Default Method", () => {
-    expect(defaultMethod()).toBe("You have called a mocked default method!");
-    expect(defaultMethod).toHaveBeenCalledTimes(1);
-  });
-});
+// import defaultMethod, { method1, method2, method3 } from "./greeterDefault";
+
+// jest.mock('./greeterDefault', () => ({
+//   __esModule: true,
+//   default: jest.fn().mockImplementation(() => 'You have called a mocked default method!'),
+//   method1: jest.fn().mockImplementation(() => 'You have called a mocked method 1!'),
+//   method2: jest.fn().mockImplementation(() => 'You have called a mocked method 2!'),
+//   method3: jest.fn().mockImplementation(() => 'You have called a mocked method 3!'),
+// }))
+
+// describe("default and named exports - module factory with mock implementation mocked file", () => {
+//   it("returns the correct value for Method 1", () => {
+//     expect(method1()).toBe("You have called a mocked method 1!");
+//     expect(method1).toHaveBeenCalledTimes(1);
+//   });
+
+//   it("returns the correct value for Method 2", () => {
+//     expect(method2()).toBe("You have called a mocked method 2!");
+//     expect(method2).toHaveBeenCalledTimes(1);
+//   });
+
+//   it("returns the correct value for Method 3", () => {
+//     expect(method3()).toBe("You have called a mocked method 3!");
+//     expect(method3).toHaveBeenCalledTimes(1);
+//   });
+
+//   it("returns the correct value for the Default Method", () => {
+//     expect(defaultMethod()).toBe("You have called a mocked default method!");
+//     expect(defaultMethod).toHaveBeenCalledTimes(1);
+//   });
+// });

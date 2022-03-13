f409d84f39124b788f4184f46d7c969fb247ba69
Nikola <nikola@example.com>
2022-03-13 01:56:39 +0000

    call method2 spy on method1

--- src/comp/greeterDefault.js
+++ src/comp/greeterDefault.js
@@ -1,9 +1,6 @@
 export const method1 = () => "You have called Method 1";
 
-export const method2 = () => {
-  const met1 = exports.method1()
-  return "You have called Method 2 " + met1; 
-};
+export const method2 = () => "You have called Method 2 " + exports.method1();
 
 export const method3 = () => "You have called Method 3";
 

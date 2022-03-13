6e262294283b193a46db8aed8e16d0503717c8f4
Nikola <nikola@example.com>
2022-03-13 03:11:38 +0000

    call method2 spy on method1

--- src/comp/greeterDefault.test.js
+++ src/comp/greeterDefault.test.js
@@ -1,23 +1,23 @@
-const myModule = jest.requireActual("./greeterDefault");
+// const myModule = jest.requireActual("./greeterDefault");
 
-it("call method2 and spy on method1", () => {
-  const spyMethod1 = jest.spyOn(myModule, "method1")
-  expect(myModule.method2()).toBe('You have called Method 2 You have called Method 1');
-  expect(spyMethod1).toHaveBeenCalledTimes(1);
-})
+// it("call method2 and spy on method1", () => {
+//   const spyMethod1 = jest.spyOn(myModule, "method1")
+//   expect(myModule.method2()).toBe('You have called Method 2 You have called Method 1');
+//   expect(spyMethod1).toHaveBeenCalledTimes(1);
+// })
 
-it("spy on default and clear spy on it", () => {
-  const spyDefault = jest.spyOn(myModule, "default").mockImplementation(() => 5);
-  expect(spyDefault()).toBe(5);
-  spyDefault.mockRestore()
-  expect(myModule.default()).toBe('You have called the Default Method');
-})
+// it("spy on default and clear spy on it", () => {
+//   const spyDefault = jest.spyOn(myModule, "default").mockImplementation(() => 5);
+//   expect(spyDefault()).toBe(5);
+//   spyDefault.mockRestore()
+//   expect(myModule.default()).toBe('You have called the Default Method');
+// })
 
-it("spy on method1 mock once", () => {
-  const spyMethod1 = jest.spyOn(myModule, "method1").mockImplementationOnce(() => 23);
-  expect(spyMethod1()).toBe(23);
-  expect(myModule.method1()).toBe("You have called Method 1");
-})
+// it("spy on method1 mock once", () => {
+//   const spyMethod1 = jest.spyOn(myModule, "method1").mockImplementationOnce(() => 23);
+//   expect(spyMethod1()).toBe(23);
+//   expect(myModule.method1()).toBe("You have called Method 1");
+// })
 
 //
 //
@@ -25,34 +25,33 @@ it("spy on method1 mock once", () => {
 //
 //
 
-// import defaultMethod, { method1, method2, method3 } from "./greeterDefault";
+import defaultMethod from "./greeterDefault";
+import * as mod from "./greeterDefault";
 
-// jest.mock('./greeterDefault', () => ({
-//   __esModule: true,
-//   default: jest.fn().mockImplementation(() => 'You have called a mocked default method!'),
-//   method1: jest.fn().mockImplementation(() => 'You have called a mocked method 1!'),
-//   method2: jest.fn().mockImplementation(() => 'You have called a mocked method 2!'),
-//   method3: jest.fn().mockImplementation(() => 'You have called a mocked method 3!'),
-// }))
+jest.mock('./greeterDefault', () => {
+  const originalModule = jest.requireActual('./greeterDefault');
+  return {
+    __esModule: true,
+    ...originalModule,
+    default: jest.fn().mockImplementation(() => 'You have called a mocked default method!'),
+    method1: jest.fn().mockImplementation(() => 'You have called a mocked method 1'),
+    // method2: jest.fn().mockImplementation(() => 'You have called a mocked method 2!'),
+    // method3: jest.fn().mockImplementation(() => 'You have called a mocked method 3!'),
+}})
 
-// describe("default and named exports - module factory with mock implementation mocked file", () => {
-//   it("returns the correct value for Method 1", () => {
-//     expect(method1()).toBe("You have called a mocked method 1!");
-//     expect(method1).toHaveBeenCalledTimes(1);
-//   });
+describe("default mocked", () => {
+  it("returns the correct value for the Default Method", () => {
+    expect(defaultMethod()).toBe("You have called a mocked default method!");
+    expect(defaultMethod).toHaveBeenCalledTimes(1);
+  });
 
-//   it("returns the correct value for Method 2", () => {
-//     expect(method2()).toBe("You have called a mocked method 2!");
-//     expect(method2).toHaveBeenCalledTimes(1);
-//   });
+  it("method1 mocked", () => {
+    expect(mod.method1()).toBe("You have called a mocked method 1");
+    expect(mod.method1).toHaveBeenCalledTimes(1);
+  });
 
-//   it("returns the correct value for Method 3", () => {
-//     expect(method3()).toBe("You have called a mocked method 3!");
-//     expect(method3).toHaveBeenCalledTimes(1);
-//   });
-
-//   it("returns the correct value for the Default Method", () => {
-//     expect(defaultMethod()).toBe("You have called a mocked default method!");
-//     expect(defaultMethod).toHaveBeenCalledTimes(1);
-//   });
-// });
+  it("method2 called", () => {
+    expect(mod.method2()).toBe("You have called Method 2 You have called Method 1");
+    expect(mod.method1).toHaveBeenCalledTimes(1);
+  });
+});

7876a9ac7def33e36a981674e801da7ca4301f2f
Nikola <nikola@example.com>
2022-03-13 01:35:38 +0000

    call method2 spy on method1

--- src/comp/greeterDefault.js
+++ src/comp/greeterDefault.js
@@ -1,9 +1,12 @@
 export const method1 = () => "You have called Method 1";
 
-export const method2 = () => "You have called Method 2";
+export const method2 = () => {
+  const met1 = exports.method1()
+  return "You have called Method 2 " + met1; 
+};
 
 export const method3 = () => "You have called Method 3";
 
-const defaultMethod = () => "You have called the Default Method " + method1() ;
+const defaultMethod = () => "You have called the Default Method";
 
 export default defaultMethod;
--- src/comp/greeterDefault.test.js
+++ src/comp/greeterDefault.test.js
@@ -1,17 +1,19 @@
 const myModule = jest.requireActual("./greeterDefault");
 
-it("call default", () => {
-   expect(myModule.default()).toBe('You have called the Default Method You have called Method 1');
+it("call method2 and spy on method1", () => {
+  const spyMethod1 = jest.spyOn(myModule, "method1")
+  expect(myModule.method2()).toBe('You have called Method 2 You have called Method 1');
+  expect(spyMethod1).toHaveBeenCalledTimes(1);
 })
 
-it("spy on default", () => {
+it("spy on default and clear spy on it", () => {
   const spyDefault = jest.spyOn(myModule, "default").mockImplementation(() => 5);
   expect(spyDefault()).toBe(5);
   spyDefault.mockRestore()
-  expect(myModule.default()).toBe('You have called the Default Method You have called Method 1');
+  expect(myModule.default()).toBe('You have called the Default Method');
 })
 
-it("spy on method1", () => {
+it("spy on method1 mock once", () => {
   const spyMethod1 = jest.spyOn(myModule, "method1").mockImplementationOnce(() => 23);
   expect(spyMethod1()).toBe(23);
   expect(myModule.method1()).toBe("You have called Method 1");

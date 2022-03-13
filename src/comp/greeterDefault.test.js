2df1c0d862f345d0051cf869ef791b5418704276
Nikola <nikola@example.com>
2022-03-13 03:27:24 +0000

    es6 mock default and named

--- src/comp/greeterDefault.test.js
+++ src/comp/greeterDefault.test.js
@@ -39,19 +39,27 @@ jest.mock('./greeterDefault', () => {
     // method3: jest.fn().mockImplementation(() => 'You have called a mocked method 3!'),
 }})
 
-describe("default mocked", () => {
+describe("default and named mocked", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("returns the correct value for the Default Method", () => {
     expect(defaultMethod()).toBe("You have called a mocked default method!");
     expect(defaultMethod).toHaveBeenCalledTimes(1);
   });
 
-  it("method1 mocked", () => {
+  it("mocked method1 called 1st time", () => {
     expect(mod.method1()).toBe("You have called a mocked method 1");
-    expect(mod.method1).toHaveBeenCalledTimes(1);
   });
 
-  it("method2 called", () => {
-    expect(mod.method2()).toBe("You have called Method 2 You have called Method 1");
-    expect(mod.method1).toHaveBeenCalledTimes(1);
+  it("mocked method1 called 2nd time", () => {
+    expect(mod.method1()).toBe("You have called a mocked method 1");
+    expect(mod.method1).toHaveBeenCalledTimes(1); // still 1 due to afterEach
   });
+
+  it("not-mocked method2 called", () => {
+    expect(mod.method2()).toBe("You have called Method 2 You have called Method 1");
+  });
+
 });

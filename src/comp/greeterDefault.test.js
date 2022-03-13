4023959574d0abd39193a2e050f2950c26fada2d
Nikola <nikola@example.com>
2022-03-13 11:15:06 +0000

    counting times called

--- src/comp/greeterDefault.test.js
+++ src/comp/greeterDefault.test.js
@@ -1,17 +1,17 @@
 const myModule = jest.requireActual("./greeterDefault");
 
 describe('my beverageeeee', () => {
-  //const spyMethod1 = jest.spyOn(myModule, "method1")
+  // const spyMethod1 = jest.spyOn(myModule, "method1")
   
   beforeEach(() => {
     
   });
 
   afterEach(() => {
-    //spyMethod1.mockRestore()
+    
   });
 
-  it("call method2 and spy on mocked method1 no mock", () => {
+  it("call method2 and spy on method1", () => {
     const spyMethod1 = jest.spyOn(myModule, "method1");
     expect(myModule.method2()).toBe('You have called Method 2 You have called Method 1');
     expect(spyMethod1).toHaveBeenCalledTimes(1);
@@ -22,6 +22,7 @@ describe('my beverageeeee', () => {
     expect(myModule.method2()).toBe('You have called Method 2 you called mocked method1');
     expect(spyMethod1).toHaveBeenCalledTimes(2);
   })
+
 
   it("spy on default and clear spy on it", () => {
     const spyDefault = jest.spyOn(myModule, "default").mockImplementation(() => 5);

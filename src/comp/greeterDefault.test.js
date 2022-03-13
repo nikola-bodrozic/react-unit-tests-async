cdbed633ccfdabd06a1c10254bf023aeec887239
Nikola <nikola@example.com>
2022-03-13 11:36:28 +0000

    afterEach reset mock

--- src/comp/greeterDefault.test.js
+++ src/comp/greeterDefault.test.js
@@ -8,14 +8,12 @@ it("spy on default, clear spy on it and call default again", () => {
 })
 
 describe('call method2 and spy on method1', () => {
-  //const spyMethod1 = jest.spyOn(myModule, "method1")
-  
   beforeEach(() => {
     
   });
 
   afterEach(() => {
-    
+    jest.clearAllMocks();
   });
 
   it("metod1 has no mock", () => {
@@ -27,6 +25,6 @@ describe('call method2 and spy on method1', () => {
   it("method1 has mock", () => {
     let hasMock = jest.spyOn(myModule, "method1").mockImplementation(() => 'you called mocked method1');
     expect(myModule.method2()).toBe('You have called Method 2 you called mocked method1');
-    expect(hasMock).toHaveBeenCalledTimes(2);
+    expect(hasMock).toHaveBeenCalledTimes(1);
   })
 })
